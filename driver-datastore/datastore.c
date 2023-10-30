feb636f59e999344dac98fd34deeef163f04b25f
Tiger Wang <tiger.wangj@gmail.com>
2023-10-29 22:27:33 -0400

    datastore driver + reader driver

---
+++ driver-datastore/datastore.c
@@ -0,0 +1,31 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+unsigned long DATA = 0;
+
+int datastore_init(void) {
+  printk(KERN_INFO "Datastore initialized!\n");
+  return 0;
+}
+
+void datastore_exit(void) {
+  printk(KERN_INFO "Shutting down datastore.\n");
+}
+
+module_init(datastore_init);
+module_exit(datastore_exit);
+
+void datastore_add(unsigned long n) {
+  DATA += n;
+}
+
+unsigned long datastore_get() {
+  return DATA;
+}
+
+EXPORT_SYMBOL(datastore_add);
+EXPORT_SYMBOL(datastore_get);
+
+MODULE_AUTHOR("Tiger Wang <tiger.wang@yale.edu>");
+MODULE_DESCRIPTION("Simple driver for an in-memory int store");
+MODULE_LICENSE("GPL");
---
+++ driver-reader/reader.c
@@ -0,0 +1,24 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+extern void datastore_add(unsigned long);
+extern unsigned long datastore_get();
+
+int reader_init(void) {
+  printk(KERN_INFO "Reader: Adding 10 to datastore and reading value\n");
+  datastore_add(10);
+  unsigned long result = datastore_get();
+  printk(KERN_INFO "Reader: Value is %lu\n", result);
+  return 0;
+}
+
+void reader_exit(void) {
+  printk(KERN_INFO "Shutting down reader.\n");
+}
+
+module_init(reader_init);
+module_exit(reader_exit);
+
+MODULE_AUTHOR("Tiger Wang <tiger.wang@yale.edu>");
+MODULE_DESCRIPTION("Simple reader of the datastore");
+MODULE_LICENSE("GPL");
